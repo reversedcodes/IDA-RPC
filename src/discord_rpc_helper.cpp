d14c16a7d81cb2d226913e9008d697a24ef3710e
Reversed Codes <echtedeveloper@gmail.com>
2025-06-14 22:52:49 +0200

    keep time stamp

--- src/discord_rpc_helper.cpp
+++ src/discord_rpc_helper.cpp
@@ -9,33 +9,40 @@
 #include <name.hpp>
 #include <bytes.hpp>
 
-extern idarpc::discord::RichPresence* rpc;
+extern idarpc::discord::RichPresence *rpc;
 
-std::string get_ida_version_string() {
+static time_t start_time = time(nullptr);
+
+std::string get_ida_version_string()
+{
     char version[32] = {0};
     get_kernel_version(version, sizeof(version));
     return std::string(version);
 }
 
-namespace idarpc::discord_rpc_helper {
-
-    void update_presence(const discord_helper_spec& spec) {
-
+namespace idarpc::discord_rpc_helper
+{
+    void update_presence(const discord_helper_spec &spec)
+    {
         if (!rpc)
             return;
 
         DiscordRichPresence presence{};
+        presence.startTimestamp = start_time;
 
-        if(idarpc::idahelper::is_ida_home_version()) {
+        if (idarpc::idahelper::is_ida_home_version())
+        {
             presence.largeImageKey = "ida_home";
             std::string homeText = "IDA HOME " + get_ida_version_string();
             presence.largeImageText = homeText.c_str();
-        } else {
+        }
+        else
+        {
             presence.largeImageKey = "ida_pro";
-            std::string proText = "IDA PRO" + get_ida_version_string();
+            std::string proText = "IDA PRO " + get_ida_version_string();
             presence.largeImageText = proText.c_str();
         }
-        
+
         presence.state = idahelper::get_filename();
         presence.details = spec.details.c_str();
         presence.smallImageKey = spec.small_image_key.c_str();
@@ -43,4 +50,5 @@ namespace idarpc::discord_rpc_helper {
 
         rpc->update_presence(presence);
     }
+
 }
